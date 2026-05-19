d269708fa854373e4db6e33b29aae8ca3499d7ad
joakimmacharia <mjoakim414@gmail.com>
2026-05-19 20:14:14 +0300

    Update Transaction1.java

--- src/Lecture1_adt/Transaction1.java
+++ src/Lecture1_adt/Transaction1.java
@@ -1,37 +1,12 @@
-package Lecture1_adt;
+package Lecture1_adt; 
 
-import java.util.Calendar;
+import java.util.Calendar; 
 
-/**
- * This Lecture1_adt.TransactionInterface Class violates several ADT design principles:
- * 1. Representation Independence: --- Changes in representation of the data may require external code to alter access
- * 2. Preservation of Invariants: --- Any external client code can alter the internal values
- */
-public class Transaction1 {
-    // private 
-    private int amount;
-    private Calendar date;
+/** * This Lecture1_adt.TransactionInterface Class violates several ADT design principles:
+* 1. Representation Independence: --- Changes in representation of the data may require external code to alter access 
 
-    public Transaction1(int amount, Calendar date) {
-       if(amount<0){
-           throw new illegalArgumentException("amount cannot be negative);
-       }
-        this.amount = amount;
-        this.date = (Calendar) date.clone();
-    }
-    public int getAmount() {
-        return amount;
-}
-    public void setAmount(int amount){
-        if(amount<0){
-            throw new illegalArgumentException("Amount cannot be negative")
-                }
-        this.amount= amount;
-    }
-    public Calender getDate(){
-        return (Calender) date.clone();
-
-        publicvoid setDate( Calender date) {
-            this.date = ( Calender) date.clone();
-    }
-}
+* 2. Preservation of Invariants: --- Any external client code can alter the internal values */ 
+public class Transaction1 { public int amount; public Calendar date;
+                           public Transaction1(int amount, Calendar date) 
+                           { this.amount = amount; 
+                            this.date = (Calendar) date.clone(); }
